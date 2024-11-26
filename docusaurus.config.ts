5d08f65b64eaa3ce3a45aea2b4a5cc9b775e2a5f
ycherabai <yu.cherabai@gmail.com>
2024-11-26 22:44:01 +0100

    initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,69 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Technical Documentation',
+  // TODO: change to company favicon
+  favicon: 'img/favicon.ico',
+
+  url: 'https://uma-hive.github.io/',
+  baseUrl: '/',
+
+  organizationName: 'uma-hive',
+  projectName: 'docs',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          editUrl:
+            'https://github.com/uma-hive/docs/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'Technical Documentation',
+      // TODO: add company logo
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        {
+          href: 'https://github.com/uma-hive/docs',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
